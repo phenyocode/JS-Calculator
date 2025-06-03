37229be13d88035af60c58140a57b374f95ec080
phenyocode <ontiretsemotlhabane6@gmail.com>
2025-06-03 17:05:22 +0200

    Remove 0 in the display

--- script.js
+++ script.js
@@ -7,7 +7,27 @@ let justCalculated = false;
 function appendToDisplay(value) {
     console.log('Button pressed:', value);
 
-    alert('You pressed:' + value);
+    let currentValue = display.value;
+
+    if (justCalculated && !isNaN(value)) {
+        display.value = value;
+        justCalculated = false;
+        return;
+    }
+
+    // If current display show 0 and user enters a number, we wanna replace the 0
+    if (currentValue === "0" && !isNaN(value)){
+        display.value = value;
+    } else if (currentValue === '0' && value === '.') {
+        display.value = currentValue + value;
+    } else {
+        display.value = currentValue + value;
+    }
+
+    // Reset the justCalculated flag when user starts typing
+    justCalculated = false;
+
+    console.log('Display updated to:', display.value);
 }
 
 function clearDisplay() {
@@ -18,6 +38,15 @@ function clearDisplay() {
 
 function deleteLast() {
     console.log('Backspace button pressed');
+
+    let currentValue = display.value;
+
+    // If theres only one character or its 0, reset to 0
+    if (currentValue.length <= 1 || currentValue === '0') {
+        display.value = '0';
+    } else {
+        display.value = currentValue.slice(0, -1);
+    }
 
     alert('Backspace button was clicked');
 }
